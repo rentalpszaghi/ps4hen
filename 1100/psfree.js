c31544b16eb18ec93389d12873d8d9d8440f4b46
bastengaming <basten29.game@gmail.com>
2025-12-07 19:59:24 +0700

    Delete 1100 directory

--- 1100/psfree.js
+++
@@ -1,1 +0,0 @@
-// placeholder psfree loader script
--- 1100/sw.js
+++
@@ -1,8 +0,0 @@
-// placeholder service worker - optional for advanced caching
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('basten-1100-v1').then(function(cache) {
-      return cache.addAll(['/ps4hen/1100/index.html']);
-    })
-  );
-});
